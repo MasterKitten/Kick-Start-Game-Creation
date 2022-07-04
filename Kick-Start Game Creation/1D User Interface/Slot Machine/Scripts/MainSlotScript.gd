da31ba15402144737446a36890899ef1bbf7da0a
MasterKitten <51054796+MasterKitten@users.noreply.github.com>
2022-07-04 15:26:09 -0700

    Update for calculator

---
+++ Kick-Start Game Creation/1D User Interface/Slot Machine/Scripts/MainSlotScript.gd
@@ -0,0 +1,26 @@
+extends Control
+
+var MoneySpent = 0.0
+var MoneyGave = 0.0
+
+export var MoneyRequired = 25.0
+
+var MoneyHave = 1000000
+# As the game progresses, the person should get only this PERCENTAGE back fron the machine
+export var PercentWin = 10.0
+
+# The LovelyNumberForRigging is used for how many slots were landed on. Yes, im rigging this :D
+func Rigged(var LovelyNumberForRigging):
+	match LovelyNumberForRigging:
+		2:
+			if (MoneyGave / MoneySpent) < (PercentWin / 100) * 2 or MoneyGave == 0:
+				MoneyHave += MoneyRequired
+				MoneyGave += MoneyRequired
+			else:
+				print("Rigged!")
+		3:
+			if MoneyGave / MoneySpent < PercentWin / 100 or MoneyGave == 0:
+				MoneyHave += MoneyRequired * 2
+				MoneyGave += MoneyRequired
+			else:
+				print("Rigged!")
