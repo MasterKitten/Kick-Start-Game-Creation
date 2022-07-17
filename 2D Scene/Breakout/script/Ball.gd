7330ec2af3062ab536f21615e243d73c2a0cb743
MasterKitten <51054796+MasterKitten@users.noreply.github.com>
2022-07-16 20:02:30 -0700

    Revert "Commit 1/2"
    This reverts commit 284a1795b0a30633435d08467ca0adae30b3ac0c.

--- 2D Scene/Breakout/script/Ball.gd
+++
@@ -1,9 +0,0 @@
-extends RigidBody2D
-
-func _on_Ball_body_entered(body):
-	# detect if this is an enemy (I like using bits)
-	if body.get_collision_layer_bit(1) and body.get_collision_mask_bit(1):
-		body.queue_free()
-
-func _physics_process(delta):
-	self.add_central_force(Vector2(0, -0.04 * delta))
